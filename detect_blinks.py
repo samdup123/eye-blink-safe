6e6b451ea471d6eb98699f196a529daa117b2f80
sam <samdup123@gmail.com>
2019-04-16 17:57:58 -0400

    blah

--- detect_blinks.py
+++ detect_blinks.py
@@ -38,16 +38,6 @@ ap.add_argument("-v", "--video", type=str, default="",
 	help="path to input video file")
 args = vars(ap.parse_args())
 
-# define two constants, one for the eye aspect ratio to indicate
-# blink and then a second constant for the number of consecutive
-# frames the eye must be below the threshold
-EYE_AR_THRESH = 0.3
-EYE_AR_CONSEC_FRAMES = 3
-
-# initialize the frame counters and the total number of blinks
-COUNTER = 0
-TOTAL = 0
-
 # initialize dlib's face detector (HOG-based) and then create
 # the facial landmark predictor
 print("[INFO] loading facial landmark predictor...")
@@ -68,6 +58,27 @@ vs = VideoStream(src=0).start()
 # vs = VideoStream(usePiCamera=True).start()
 time.sleep(1.0)
 
+EAR_THRESH = .26
+EAR_CONSEC_FRAMES = 3
+
+frame_counter_left_eye = 0
+frame_counter_right_eye = 0
+
+attempt_in_progress = False
+
+blink_pattern = ['left', 'left', 'right', 'right', 'left', 'left', 'right', 'right', 'left', 'left', 'right', 'right']
+
+blink_count = 0
+
+def register_blink(eye):
+	print('blink occured ' + eye)
+	global blink_count
+	global blink_pattern
+	blink_count+=1
+
+	return blink_pattern[blink_count - 1] == eye
+
+
 # loop over frames from the video stream
 while True:
 	# if this is a file video stream, then we need to check if
@@ -86,11 +97,11 @@ while True:
 	rects = detector(gray, 0)
 
 	if len(rects) > 1:
-		cv2.putText(frame, "Only one person can be on screen!".format(TOTAL), (10, 150),
+		cv2.putText(frame, "Only one person can be on screen!", (10, 150),
 			cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+	elif len(rects) == 1:
+		rect = rects[0]
 
-	# loop over the face detections
-	for rect in rects:
 		# determine the facial landmarks for the face region, then
 		# convert the facial landmark (x, y)-coordinates to a NumPy
 		# array
@@ -104,8 +115,31 @@ while True:
 		leftEAR = eye_aspect_ratio(leftEye)
 		rightEAR = eye_aspect_ratio(rightEye)
 
-		# average the eye aspect ratio together for both eyes
-		ear = (leftEAR + rightEAR) / 2.0
+		cv2.putText(frame, "Left ear: {:.2f}, Right ear: {:.2f}".format(leftEAR, rightEAR), (10, 30),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+		if (not attempt_in_progress) and (leftEAR > .34) and (rightEAR > .34):
+			print('started')
+			attempt_in_progress = True
+
+
+		if leftEAR < EAR_THRESH:
+			frame_counter_left_eye += 1
+		else:
+			if frame_counter_left_eye >= EAR_CONSEC_FRAMES:
+				register_blink('left')
+
+			# reset the eye frame counter
+			frame_counter_left_eye = 0
+
+		if rightEAR < EAR_THRESH:
+			frame_counter_right_eye += 1
+		else:
+			if frame_counter_right_eye >= EAR_CONSEC_FRAMES:
+				register_blink('right')
+
+			# reset the eye frame counter
+			frame_counter_right_eye = 0
 
 		# compute the convex hull for the left and right eye, then
 		# visualize each of the eyes
@@ -113,29 +147,6 @@ while True:
 		rightEyeHull = cv2.convexHull(rightEye)
 		cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
 		cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
-
-		# check to see if the eye aspect ratio is below the blink
-		# threshold, and if so, increment the blink frame counter
-		if ear < EYE_AR_THRESH:
-			COUNTER += 1
-
-		# otherwise, the eye aspect ratio is not below the blink
-		# threshold
-		else:
-			# if the eyes were closed for a sufficient number of
-			# then increment the total number of blinks
-			if COUNTER >= EYE_AR_CONSEC_FRAMES:
-				TOTAL += 1
-
-			# reset the eye frame counter
-			COUNTER = 0
-
-		# draw the total number of blinks on the frame along with
-		# the computed eye aspect ratio for the frame
-		cv2.putText(frame, "Blinks: {}".format(TOTAL), (10, 30),
-			cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-		cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
-			cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
 	# show the frame
 	cv2.imshow("Frame", frame)
