15550df89ecf2b7737aaeb7630ba74feadad835a
Sam <samdup123@gmail.com>
2019-04-30 11:14:31 -0400

    Update detect_blinks.py

--- detect_blinks.py
+++ detect_blinks.py
@@ -1,3 +1,5 @@
+# detect_blinks.py
+
 # USAGE
 # python detect_blinks.py --shape-predictor shape_predictor_68_face_landmarks.dat --video blink_detection_demo.mp4
 # python detect_blinks.py --shape-predictor shape_predictor_68_face_landmarks.dat
